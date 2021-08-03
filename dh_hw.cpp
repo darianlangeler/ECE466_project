a447fdab2488a7515118023c507418f768801db4
darianlangeler <48569079+darianlangeler@users.noreply.github.com>
2021-08-02 17:54:57 -0700

    moved computation to hw, updated hw controller states

--- dh_hw.cpp
+++ dh_hw.cpp
@@ -5,9 +5,12 @@
 #define WAIT 0
 #define INPUT 1
 #define EXECUTE 2
-#define BONUS 3
-#define OUTPUT 4
-#define FINISH 5
+#define E1 3
+#define E2 4
+#define E3 5
+#define BONUS 6
+#define OUTPUT 7
+#define FINISH 8
 
 void dh_hw::process_hw()
 {
@@ -30,23 +33,30 @@ void dh_hw::process_hw()
           break;
         case INPUT:
           load.write(true);
+          state = EXECUTE;
           break;
         case EXECUTE:
+          load.write(false);
+          comp_mux_control.write(comp_out.read());
+          state = E1;
+          break;
+        case E1:
+          sub_comp_mux_control.write(true);
+          state = E2;
+          break;
+        case E2:
+          sub_comp_mux_control.write(false);
+          sub_uhh_mux_control.write(true);
+          state = E3;
+          break;
+        case E3:
+          sub_uhh_mux_control.write(false);
+          sub_v_mux_control.write(true);
+          // Get values for bonus
           t[0] = from_sw0.read();
           t[1] = from_sw1.read();
           c = from_sw2.read();
           aHigh = from_sw3.read();
-          /*** Begin: Required part (to do: Datapath + Control) ***/
-          cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
-          cLow = (NN_HALF_DIGIT)LOW_HALF (c);
-      
-          u = (NN_DIGIT)aHigh * (NN_DIGIT)cLow;
-          v = (NN_DIGIT)aHigh * (NN_DIGIT)cHigh;
-          
-          if ((t[0] -= TO_HIGH_HALF (u)) > (MAX_NN_DIGIT - TO_HIGH_HALF (u))) t[1]--;
-          
-          t[1] -= HIGH_HALF (u);
-          t[1] -= v;
           state = BONUS;
           break;
         case BONUS:
